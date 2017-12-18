3a9d2af00eee7ce4a606fb623bd2ffe256927f9e
Manjeet Singh <manjeetbrar91@gmail.com>
2017-12-18 13:02:50 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
+ * Front end to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package WordPress
+ * @package WordPress 
  */
 
 /**
